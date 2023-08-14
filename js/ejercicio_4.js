fd636a2bd0705c478d25971c96e9c07ed308bf2c
claudiapaolasalinas <130775654+claudiapaolasalinas@users.noreply.github.com>
2023-08-14 17:03:23 -0300

    Add files via upload

---
+++ js/ejercicio_4.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+
+let acumulador = 1;
+
+acumulador = acumulador + 19;
+
+acumulador = acumulador * 2;
+
+acumulador = acumulador + 20;
+
+acumulador = acumulador * 2;
+
+acumulador = acumulador - 20;
+
+console.log("El resultado del programa es: " + acumulador); 
+
